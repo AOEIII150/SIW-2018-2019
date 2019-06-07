6328653514509c87977d91b3e613d2be09f03b44
AOEIII150 <symo735@gmail.com>
2019-06-07 14:54:52 +0200

    Update StudenteRepository.java

--- src/main/java/it/uniroma3/siw/silphspa/repository/StudenteRepository.java
+++ src/main/java/it/uniroma3/siw/silphspa/repository/StudenteRepository.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Repository;
 import it.uniroma3.siw.silphspa.model.*;
 
 @Repository
-public interface StudenteRepository extends CrudRepository<Studente, Long>{
+public interface StudenteRepository extends CrudRepository<Fotografo, Long>{
 	//findBy
-	public List<Studente> findByNome(String nome);
-	public List<Studente> findByNomeAndCognome(String nome, String cognome);
-	public List<Studente> findByMatricola(Long matricola);
+	public List<Fotografo> findByNome(String nome);
+	public List<Fotografo> findByNomeAndCognome(String nome, String cognome);
+	public List<Fotografo> findByMatricola(Long matricola);
 	
 }
