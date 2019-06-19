9023740160f649182acb53419d711937a230bfca
Martina <martinella17@gmail.com>
2019-06-19 17:18:20 +0200

    Mha

--- src/main/java/it/uniroma3/siw/silphspa/controller/SystemController.java
+++ src/main/java/it/uniroma3/siw/silphspa/controller/SystemController.java
@@ -40,7 +40,6 @@ public class SystemController {
 		Random random = new Random();
         int index = random.nextInt(fotos.size());
         index++;
-        System.out.println("XXXXX = " +index);
 		Foto f = this.fotoService.findById(index);
 		model.addAttribute("foto", f);
 		Fotografo fotografo = f.getFotografo();
