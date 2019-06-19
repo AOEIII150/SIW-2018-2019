41422ccd3521e282dbc3c4a1b1c613eac0818166
Martina <martinella17@gmail.com>
2019-06-19 19:14:31 +0200

    mostraRichiesta

--- src/main/java/it/uniroma3/siw/silphspa/services/RichiestaService.java
+++ src/main/java/it/uniroma3/siw/silphspa/services/RichiestaService.java
@@ -7,8 +7,6 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import it.uniroma3.siw.silphspa.model.Foto;
 import it.uniroma3.siw.silphspa.model.Richiesta;
 import it.uniroma3.siw.silphspa.repository.RichiestaRepository;
 
